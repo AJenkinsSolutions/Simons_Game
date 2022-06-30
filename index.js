64574b2a9086cc6af6b4a47c4381eeaf217c37f3
AJenkinsSoultions <ajenkinsuiw@gmail.com>
2022-06-30 13:50:00 -0500

    Add Refactor Entire code base, Add dev Notes

--- index.js
+++ index.js
@@ -1,146 +1,119 @@
-//Game Active
-let gameOn = false;
+
 //Tiles Choices
 const tiles = ['green', 'red', 'yellow', 'blue'];
+//Game Active
+let gameOn = false;
 // Random pattern of tiles
-let pattern = [];
+let gamePattern = [];
 // list of tiles users has clicked
 let playerPattern = [];
-// number of clicks
-let clicks = 0;
+
 let level = 0;
 
+    
+// ==========================MainLoop=======================================
 
-function startGame(){
-    gameOn = true;
-    // then toggle our game state to True
-    $(document).keydown(function(e){game()});
-}
-function game(){
-        console.log('game start')
-        playerPattern = [];
-        console.log('player pattern', playerPattern)
-        changeLevel();
-        let currentTile = pattern[level];
-        //playsound
-        setTimeout(() =>{playSound(currentTile)},1000)
-        console.log('current tile', currentTile)
-        console.log('current level', level +1);
-        playClickSound();
-}
+    // Event Listeners 
+    // Inital KeyDown detection
 
-function changeLevel(){
-    //Change title
-    $('#level-title').text('level:' + (level + 1));
-
-}
-function GeneratePattern(size){
-
-    for(let i=0; i<size; i++){
-    //Chose random index
-    let choiceIndex = Math.floor(Math.random() * tiles.length);
-    //Get index tile 
-    switch (choiceIndex) {
-        case 0:
-            //Append item to array
-            pattern.push('green');
-            break;
-        case 1:
-            pattern.push('red');
-            break;
-        case 2:
-            pattern.push('yellow');
-            break;
-        case 3:
-            pattern.push('blue');
-            break;
-        default:
-            break;
-    }
-    }
-    console.log('debug patten Array:', pattern);
-    }
-
-function playSound(tile){
-    //takes input of current tile 
-            let currentSound = new Audio('sounds/'+ tile+'.mp3');
-            currentSound.play();
-            animateButton(tile);
-}
-function animateButton(tile){
-    $('#' + tile).addClass('pressed');
-            setTimeout( ()=> {
-                $('#' + tile).removeClass('pressed');
-            }, 100);
-
-}
-function playClickSound(){
-
-    $('.btn').click(e => {
-        
-        let target = e.target.id;
-        if(target){
-            animateButton(target);
-            playSound(target);
-            console.log(target);
-            playerPattern.push(target);
-
+    $(document).keydown(() => {
+        //we only want this option avaible if the game hasnt started yet therefore
+        //Change title to current level
+        // Invoke next sequence 
+        //Change Game state to ON
+        if(!gameOn){
+            $('#level-title').text('level:' + (level));
+            nextSequence();
+            gameOn = true; 
         }
-        
-        
-        
-        // Add click target to player array
-        console.log('post clickpattern:', playerPattern)
+    });
 
+    $('.btn').click(function() {
+        //Get users chossen click
+        //Using 'this' rather than e.id 
+        let userColorChoice = $(this).attr("id");
+        // console.log('(Debug): userColorChoice:',userColorChoice);
+        //push users chossen color to array
+        playerPattern.push(userColorChoice);
+        console.log('(Debug): PlayerPattern:',playerPattern);
 
-        if(playerPattern.length == (level+1)){
-            console.log('player patten and game pattern are the same length')
-            console.log('player pattern:', playerPattern.length);
-            //if player patten length and level are equal we can compare our arrays 
-
-            if(checkPattern){
-                console.log('next round empty players arrays change level ')
-                //Empty player patten
-                
-                level ++
-                playerPattern = [];
-                console.log('player array:', playerPattern)
-                console.log('level increased', level)
-                // now we need to loop again doing all the same thing but the next iteration in our pattern 
-                
-            }
-
-        }
+        //play sounds and animate
+        playSound(userColorChoice);
+        animateButton(userColorChoice);
+        //Check out answer 
+        checkAnswer(playerPattern.length-1)
 
     })
-    return true
 
-}
-function checkPattern(){
-
-    for(let i=0; i<=level; i++){
-        console.log('start');
-        if(playerPattern[i] == pattern[i]){
-            console.log('iteration '+ i +  'good' )
-    
-        }else{
-            console.log('iteration '+ i +  'Notgood' )
-            gameOn = false;
-            // this where we want to end our game
-            // add our endgame functionsality
-            console.log('Game oveer');
-            return false
-           
+function checkAnswer(currentLevel){
+    //? title match ??
+    if(playerPattern[currentLevel] == gamePattern[currentLevel]){
+        //?? both arrays equal length??
+        if(playerPattern.length == gamePattern.length){
+            setTimeout(function(){
+                nextSequence();
+            }, 1000)
         }
-        console.log('end');
-    }
-    // wont run loop is successful
-    console.log('loop ended without failure meaning player completed level');
-    // if successful we need to empty the players patten for
-    playerPattern = [];
-    return true
 
+    }else{
+        //Play Sound
+        //Change background
+        //Change Title
+        //Remnove Class after Interval
+        //Invoke Restart
+        playSound('wrong');
+        
+        $('body').addClass('game-over');
+        
+        $('#level-title').text('Game Over: Press Any Key to Play Again');
+        
+        setTimeout(function(){
+            $('body').removeClass('game-over');
+        }, 500)
+        
+        restartGame();
+    }
 }
 
-GeneratePattern(5);
-startGame();
+function nextSequence(){
+        //Reassign player Pattern to Empty
+        //increment the level
+        //Display level
+        //Get random index
+        //Add tile to game pattern
+        //Add Custom Animation
+        //Play Sound
+
+        playerPattern = [];
+        level ++;
+        $('#level-title').text('level:' + (level));
+        
+        let choiceIndex = Math.floor(Math.random() * tiles.length);
+        // console.log('(Debug): choiceIndex: ', choiceIndex);
+        let randomTileChoice = tiles[choiceIndex];
+        // console.log('(Debug): randomTileChoice:', randomTileChoice);
+        gamePattern.push(randomTileChoice);
+        // console.log('(Debug): gamepattern:', gamePattern);
+
+        //Vfx, Sfx
+        $('#'+randomTileChoice).fadeIn(100).fadeOut(100).fadeIn(100);
+        playSound(randomTileChoice);
+    }
+function animateButton(tile){
+        $('#' + tile).addClass('pressed');
+        setTimeout(()=> {
+            $('#' + tile).removeClass('pressed');
+        }, 100);
+        }
+function playSound(tile){
+        let currentSound = new Audio('sounds/'+ tile+'.mp3');
+        currentSound.play();
+    }
+function restartGame(){
+    gamePattern = [];
+    level = 0; 
+    gameOn = false; 
+    //We are not actually resetting the game 
+    //We resetting all the game variables
+    //Therefore our first event listerner will invoke the start of the game now our gameOn varaibles is now false
+}
